20a76b08129ed1425e15c7a7fd075ece80f439f6
Veronika <2026yarova.veronika@fcboe.org>
2024-01-24 13:55:44 -0500

    Completed

--- c/scanf.c
+++ c/scanf.c
@@ -4,12 +4,13 @@ int main()
 	int age;
 	float gpa;
 	char grade;
+
 	printf("Enter your age: ");
 	scanf("%i", &age);
 	printf("Enter your gpa: ");
 	scanf("%f", &gpa);
 	printf("Enter your letter grade: ");
-	scanf("%c", &grade);
+	scanf(" %c", &grade);
 	printf("Your age %i\n Your gpa %.2f\n Your letter grade: %c\n", age, gpa, grade);
 	return 0;
 }
